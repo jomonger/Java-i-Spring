2a7627b259ebb498d963c799214e7cf7b3fd6245
R <adasdasda@sda.sa>
2019-11-16 05:22:10 +0100

    Wersja 1.02

--- Launcher_JPaneli/Pliki_Podstawowe/Okno.java
+++ Launcher_JPaneli/Pliki_Podstawowe/Okno.java
@@ -5,14 +5,14 @@ public final class Okno extends JFrame{//Klasa tworzy nowe okno z przekazanym pa
 	private static final long serialVersionUID = 1242363695078327910L;
 	
 	protected Okno(Panel panel) {
-				super(panel.getClass().getName());// Nazywa okno tak samo jak nazwa klasy wyœwietlanego panelu.
-				setResizable(true);
-				getContentPane();
-				Container cp = getContentPane();
-				cp.add(panel);
-				pack();
-				setDefaultCloseOperation(panel.getClose());//2 - JFrame.DISPOSE_ON_CLOSE, 3 - JFrame.EXIT_ON_CLOSE
-				setVisible(true);
+		super(panel.getClass().getName());// Nazywa okno tak samo jak nazwa klasy wyœwietlanego panelu.
+		setResizable(true);
+		getContentPane();
+		Container cp = getContentPane();
+		cp.add(panel);
+		pack();
+		setDefaultCloseOperation(panel.getClose());//2 - JFrame.DISPOSE_ON_CLOSE, 3 - JFrame.EXIT_ON_CLOSE
+		setVisible(true);
 	}
 }
  
